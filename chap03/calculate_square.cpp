384434aefc5ac17a31976ec47d5df3bede986219
huyvawn <63355258+huyvawn@users.noreply.github.com>
2024-04-09 23:35:17 +0700

    add new project

---
+++ chap03/calculate_square.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Admins on 4/9/2024.
+//
+#include <stdio.h>
+#include "calculate_square.h"
+int main(){
+    int a,sqr;
+    printf("Enter your number:");
+    scanf("%d",&a);
+    sqr = a*a;
+    printf("The square of that number is: %d", sqr);
+
+}
+
---
+++ chap03/calculate_square.h
@@ -0,0 +1,14 @@
+//
+// Created by Admins on 4/9/2024.
+//
+
+#ifndef UNTITLED_CALCULATE_SQUARE_H
+#define UNTITLED_CALCULATE_SQUARE_H
+
+
+class calculate_square{
+
+};
+
+
+#endif //UNTITLED_CALCULATE_SQUARE_H
